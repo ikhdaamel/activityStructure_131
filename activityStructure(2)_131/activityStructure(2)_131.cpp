374e90633ba818e8fc8f2f3e28552f046ce22604
ikhdaamel <ikhda.new45@gmail.com>
2023-04-03 10:47:41 +0700

    menampilkan fungsi utama

--- activityStructure(2)_131/activityStructure(2)_131.cpp
+++ activityStructure(2)_131/activityStructure(2)_131.cpp
@@ -14,3 +14,19 @@ struct Mahasiswa
 	AlamatDetail alamat;
 	int umur
 };
+
+int main() {
+	mahasiswa mhs;
+
+	cout << "masukan NIM :";
+	cin >> mhs.NIM;
+	cout << "masukan nama :";
+	cin >> mhs.nama;
+	cout << "alamat :";
+	cout << "\tMasukan desa :";
+	cin >> mhs.alamat.desa;
+	cout << "\tMasukan kota :";
+	cin >> mhs.alamat.kota;
+	cout << "masukan umur :";
+	cin >> mhs.umur;
+}
